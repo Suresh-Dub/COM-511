0419bf367308d490475373e54b9a766421a768a5
Suresh-Dub <100891096+Suresh-Dub@users.noreply.github.com>
2023-10-17 12:26:36 +0530

    Update Exp - 4.py

--- Exp - 4.py
+++ Exp - 4.py
@@ -1,32 +1,36 @@
-Creating a binary and linear search algorithm in Python without using Python is a bit challenging, but I can provide you with a pseudo-code representation of both algorithms in a language-agnostic manner. You would need to adapt these algorithms to the programming language of your choice.
-
-Here's a pseudo-code for a linear search:
-
-```plaintext
-function linear_search(arr, target):
-    for i from 0 to length(arr) - 1:
-        if arr[i] == target:
-            return i
-    return -1
-```
-
-And here's a pseudo-code for a binary search:
-
-```plaintext
-function binary_search(arr, target):
-    left = 0
-    right = length(arr) - 1
-
-    while left <= right:
-        mid = (left + right) / 2
-        if arr[mid] == target:
-            return mid
-        else if arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return -1
-```
-
-You can translate these pseudo-codes into your language of choice to create functional binary and linear search algorithms.
+#Linear search  
+ list = [1,2,3,4,5,6,7,8,9] 
+ length = len(list) 
+ x = int(input("Enter the target element for linear search:")) 
+  
+ for i in range(length + 1): 
+     if(x==i): 
+         print("Linear Search: The element is found at index",i) 
+         break 
+     elif(i==length): 
+         print('The element is not found')  
+  
+  
+ #Binary Search 
+  
+ my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9] 
+ x = int(input("Enter the target element for binary search: ")) 
+  
+ low = 0 
+ high = len(my_list) - 1 
+  
+ found = False  # Initialize a variable to keep track of whether the element was found or not 
+  
+ while low <= high: 
+     mid = (low + high) // 2 
+     if x == my_list[mid]: 
+         print("Binary Search: The element is found at index", mid) 
+         found = True 
+         break 
+     elif x < my_list[mid]: 
+         high = mid - 1 
+     else: 
+         low = mid + 1 
+  
+ if not found: 
+     print('Binary Search: The element is not found')
